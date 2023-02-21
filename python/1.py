805deafb5a8242f9944c7b789ae275e22caf8ee4
Dheeraj kumar <115606370+kumardj2001@users.noreply.github.com>
2023-02-21 15:19:02 +0530

    Add files via upload

---
+++ python/1.py
@@ -0,0 +1,5 @@
+a=int(input("Please Enter a Number : "));  
+if(a&1==1):  
+    print("Odd")    
+else:  
+    print("Even") 
---
+++ python/4.py
@@ -0,0 +1,3 @@
+a = int(input("Enter the money saved year :"))
+for i in range (0, 4):
+    print("Money saved on " + str(i+1) + "Year : " + str((i+1)*a) )
